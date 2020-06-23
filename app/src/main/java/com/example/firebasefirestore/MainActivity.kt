ce2f2e6ec3336de3b37e82dd1bfe1cb96e4632f9
edmayen <ed_mayen@hotmail.com>
2020-06-23 13:58:52 -0500

    Retrieving Data

--- app/src/main/java/com/example/firebasefirestore/MainActivity.kt
+++ app/src/main/java/com/example/firebasefirestore/MainActivity.kt
@@ -2,14 +2,17 @@ package com.example.firebasefirestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import kotlin.text.StringBuilder
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,6 +28,27 @@ class MainActivity : AppCompatActivity() {
             val person = Person(fistName, lastName, age)
             savePerson(person)
         }
+
+        btnRetrieveData.setOnClickListener {
+            retrievePersons()
+        }
+    }
+
+    private fun retrievePersons(){
+        val querySnapshot = personCollectionRef.get()
+        val sb = StringBuilder()
+        querySnapshot
+            .addOnSuccessListener { documents ->
+                for (document in documents){
+                    val person = document.toObject<Person>()
+                    sb.append("$person\n")
+                }
+                tvPersons.text = sb.toString()
+            }
+            .addOnFailureListener { exception ->
+                Log.e("MainActivity", "ERROR GETTING DOCUMENTS: ", exception)
+            }
+
     }
 
     private fun savePerson(person: Person) = CoroutineScope(Dispatchers.IO).launch {
